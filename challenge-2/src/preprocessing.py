5bcc4c21474d2d9098040d135f8121c145f62c04
Sarvesh <135235123+Sarveshero3@users.noreply.github.com>
2025-05-25 22:35:47 +0530

    Update preprocessing.py

--- challenge-2/src/preprocessing.py
+++ challenge-2/src/preprocessing.py
@@ -2,7 +2,7 @@
 Author: Annam.ai IIT Ropar
 Team Name: SoilClassifiers
 Team Members: Member-1, Member-2, Member-3, Member-4, Member-5
-Leaderboard Rank: <Your Rank>
+Leaderboard Rank: 120
 """
 
 import os
